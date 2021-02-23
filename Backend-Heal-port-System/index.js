e187997a1d65b9f023ae08b9d107a5002a8acb83
codeHunters2021 <78640259+codeHunters2021@users.noreply.github.com>
2021-02-23 09:35:35 +0530

    Created index.js file for backend and tested connection with mongo database and installed necessary packeges

---
+++ Backend-Heal-port-System/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Express app
+const app = express();
+
+// Server Running port number
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+
+// Mongo database url
+const dbURI = "mongodb+srv://heal-port-admin:lj6q6meogzEBrTBB@heal-port-system.b4i5i.mongodb.net/Authentication-Base?retryWrites=true&w=majority";
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Connection success message
+mongoose.connection.on('connected', () => {
+    console.log("Connected to Database ...");
+});
+
+
+// Connection error message
+mongoose.connection.on('error', () => {
+    console.log("An Error has occurred", err);
+});
+
+
+// Notifying that server is listing on port 3000...
+app.listen(PORT, () => {
+    console.log("Server running on " + PORT);
+});
