3407700dd4a0066cd2ad53175231fd4ad4285e20
codeHunters2021 <78640259+codeHunters2021@users.noreply.github.com>
2021-02-23 12:22:55 +0530

    added token files to verify payload for patient and staff

---
+++ Backend-Heal-port-System/Middleware/patientToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const { use } = require('../Routes/patientRoutes');
+const Patient = mongoose.model('Patient');
+
+const jwtKey = "asadsafwafa";
+
+module.exports = (req, res, next) => {
+
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).send({ error: "You must be logged in" });
+    }
+
+    const token = authorization.replace("Bearer ", "");
+
+    jwt.verify(token, jwtKey, async(err, payload) => {
+        if (err) {
+            return res.status(401).send({ error: "You must be logged in" });
+        }
+
+        const { userId } = payload;
+        const patient = await Patient.findById(userId);
+        req.Patient = patient;
+        next();
+    });
+}
