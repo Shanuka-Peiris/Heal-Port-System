8f525b6a2f1a23011506be30d576e4651f47d384
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2021-03-23 13:00:56 +0530

    Flask api for deep learning part (Final)

--- X-ray images api/x-ray_image_api.py
+++ X-ray images api/x-ray_image_api.py
@@ -1,6 +1,10 @@
 import flask
 from flask import request, render_template
 from werkzeug.utils import secure_filename
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
 
 application = flask.Flask(__name__)
 application.config["DEBUG"] = True
@@ -11,7 +15,21 @@ application.config["DEBUG"] = True
 def upload_file():
 	if request.method == "POST":
 		temp_img = request.files["file"]
-		temp_img.save(secure_filename("image.jpeg"))  
+		temp_img.save(secure_filename("temp_image.jpeg"))  
+		
+		
+		img = image.load_img("image.jpeg",target_size=(224,224))
+		img = np.asarray(img)
+		plt.imshow(img)
+		img = np.expand_dims(img, axis=0)
+
+
+		saved_model = load_model("modelSave.h5")
+		output = saved_model.predict(img)
+		if output[0][0] > output[0][1]:
+			return "NORMAL"
+		else:
+			return "PNEUMONIA"
 	  	
 
 application.run()
