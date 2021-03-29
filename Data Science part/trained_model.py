5d4ceb0b94b822428fdaa9dbc337948a160d7655
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2021-03-29 22:51:57 +0530

    Add comments

--- Data Science part/trained_model.py
+++ Data Science part/trained_model.py
@@ -12,7 +12,7 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from glob import glob
 import matplotlib.pyplot as plt
 
-
+#Initialize paths of Datasets
 training_dataset = "/content/drive/MyDrive/Datasets/train"
 testing_dataset = "/content/drive/MyDrive/Datasets/test"
 training_folders = glob("/content/drive/MyDrive/Datasets/train/*")
@@ -20,7 +20,7 @@ training_folders = glob("/content/drive/MyDrive/Datasets/train/*")
 x_ray_width = 224
 x_ray_height = 224
 
-
+#Load the datasets with pre-processing and image augmentation
 training_augmentation = ImageDataGenerator(
                                   rescale = 1./255,
                                   fill_mode = "nearest",
@@ -34,14 +34,14 @@ testing_augmentation = ImageDataGenerator(
                                     fill_mode = "nearest"
                                     )
 
-
+#Create training dataset to feed into the training model
 train_datagen = training_augmentation.flow_from_directory(
                                                 training_dataset,
                                                 target_size = (x_ray_width, x_ray_height),
                                                 batch_size = 27,
                                                 class_mode = "categorical"
                                                 )
-
+#Create testing dataset to feed into the training model
 test_datagen = testing_augmentation.flow_from_directory(
                                             testing_dataset,
                                             target_size =  (x_ray_width, x_ray_height),
@@ -49,20 +49,24 @@ test_datagen = testing_augmentation.flow_from_directory(
                                             class_mode = "categorical"
                                             )
 
-
+#Initialize VGG16 model
 vgg_network = VGG16(input_shape = (x_ray_width, x_ray_height, 3), weights = "imagenet", include_top = True)
 
 vgg_network.summary()
 
+#To not retrain the pre-trained layers
 for layers in (vgg_network.layers)[:19]:
         print(layers)
         layers.trainable = False
 
-
+#Customized last layers
 X= vgg_network.layers[-2].output
 prediction = Dense(len(training_folders), activation = "softmax")(X)
+
+#Declare an object model
 vgg_network_final = Model(inputs = vgg_network.input, outputs = prediction)
 
+#Compile the object model
 vgg_network_final.compile(
         loss = "categorical_crossentropy", 
         optimizer = optimizers.SGD(lr = 0.0001, momentum = 0.9), 
@@ -71,7 +75,7 @@ vgg_network_final.compile(
 
 vgg_network_final.summary()
 
-
+#Declare ModelCheckpoint method to save best models
 model_check = ModelCheckpoint(
         "modelSave.h5", 
         monitor = "val_accuracy", 
@@ -82,6 +86,7 @@ model_check = ModelCheckpoint(
         period = 1
         )
 
+#Declare EarlyStopping method to identify model improvements and stop training the model
 previous = EarlyStopping(
         monitor = "val_accuracy", 
         min_delta = 0, 
@@ -90,6 +95,7 @@ previous = EarlyStopping(
         mode = "auto"
         )
 
+#Pass the train and test data to fit_generator
 final_model = vgg_network_final.fit_generator(
         generator = train_datagen, 
         steps_per_epoch = len(train_datagen), 
@@ -99,9 +105,10 @@ final_model = vgg_network_final.fit_generator(
         callbacks = [model_check, previous]
         )
 
+#Save the trained model
 vgg_network_final.save("modelSave.h5")
 
-
+#Visualise training, validation accuracy and loss using matplotlib
 plt.plot(final_model.history["accuracy"])
 plt.plot(final_model.history["val_accuracy"])
 plt.plot(final_model.history["loss"])
